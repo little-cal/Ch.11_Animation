3a5b31868016a1bb6684f968a7227381ada3ea34
little-cal <littlecal@urbandaleschools.com>
2024-02-07 08:03:27 -0600

    1/22/2024

--- 11.0_Jedi_Training.py
+++ 11.0_Jedi_Training.py
@@ -7,24 +7,25 @@
 CHAPTER 11 FINAL CODE QUESTIONS: (10pts)
 --------------------------------
 1.) Where is the ball's original location?
-
+(320, 240)
 2.) What are the variables dx and dy?
-
+How many pixels the x and y position should move when updated
 3.) How many pixels/sec does the ball move in the x-direction?
-
+180 pixels/second
 4.) How many pixels/sec does the ball move in the y-direction?
-
+120 pixels/second
 5.) Which method is run 60 times/second?
-
+delta time, on update
 6.) What does this code do?   self.dx *= -1
-
+'reverses' the direction of the x value being updated. mimics a bounce
 7.) What does this code do?  self.pos_y += self.dy
-
+adds the dy variable to the y position
 8.) What is the width of the window?
-
+640 pixels
 9.) What is this code checking?  self.pos_y > SH - self.rad:
-
+if the y position is greater than the screen height minus the radius
 10.) What is this code checking? if self.pos_x < self.rad
+if the x position is less than the radius, key in bouncing off the edge of screen
 '''
 
 
@@ -56,7 +57,88 @@ Helpful Hints:
 4.) Also in the on_draw section draw the side rails.
 5.) In the on_update section use: for box in self.boxlist: box.update_box()
 '''
+import arcade
+import random
 
+SW = 600
+SH = 600
+#
+#
+# class Box:
+#     def __init__(self, left, right, top, bottom, dx, dy, col):
+#         self.left = left
+#         self.right = right
+#         self.top = top
+#         self.bottom = bottom
+#         self.dx = dx
+#         self.dy = dy
+#         self.col = col
+#
+#     def draw_box(self):
+#         arcade.draw_lrtb_rectangle_filled(self.left, self.right, self.top, self.bottom, self.col)
+#
+#     def update_box(self):
+#         self.top += self.dy
+#         self.bottom += self.dy
+#
+#         self.left += self.dx
+#         self.right += self.dx
+#
+#         #bounce off edge of screen
+#
+#         if self.left <= 30:
+#             self.dx *= -1
+#             self.col = arcade.color.RED
+#         elif self.right >= SW - 30:
+#             self.dx *= -1
+#             self.col = arcade.color.BLUE
+#         if self.top >= SH - 30:
+#             self.dy *= -1
+#             self.col = arcade.color.YELLOW
+#         elif self.bottom <= 30:
+#             self.dy *= -1
+#             self.col = arcade.color.GREEN
+#
+#
+# class MyGame(arcade.Window):
+#     def __init__(self, width, height, title):
+#         super().__init__(width, height, title)
+#         arcade.set_background_color(arcade.color.ASH_GREY)
+#         self.box_list = []
+#         for i in range(30):
+#             left = random.randint(30, 520)
+#             right = left + random.randint(10, 50)
+#             bottom = random.randint(30, 520)
+#             top = bottom + random.randint(10, 50)
+#             x = random.randint(-5, 5)
+#             y = random.randint(-5, 5)
+#             if x == 0 or y == 0:
+#                 x = random.randint(-5, 5)
+#                 y = random.randint(-5, 5)
+#             self.box = Box(left, right, top, bottom, x, y, arcade.color.BLACK)
+#             self.box_list.append(self.box)
+#
+#     def on_draw(self):
+#         arcade.start_render()
+#         arcade.draw_rectangle_filled(585, 300, 30, 600, arcade.color.BLUE)
+#         arcade.draw_rectangle_filled(15, 300, 30, 600, arcade.color.RED)
+#         arcade.draw_rectangle_filled(300, 585, 600, 30, arcade.color.YELLOW)
+#         arcade.draw_rectangle_filled(300, 15, 600, 30, arcade.color.GREEN)
+#         for box in self.box_list:
+#             box.draw_box()
+#
+#     def on_update(self, dt):
+#         for box in self.box_list:
+#             box.update_box()
+#
+#
+# def main():
+#     window = MyGame(SW, SH, "30 Boxes")
+#     arcade.run()
+#
+#
+# if __name__ == "__main__":
+#     main()
 
 
 
@@ -79,3 +161,70 @@ the following requirements:
 9.) Color snowflake #1 red just for fun.
 10.) All other snowflakes should be white.
 '''
+
+
+class Snowflake:
+    def __init__(self, pos_x, pos_y, dy, dx, rad, col):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.dy = dy
+        self.dx = dx
+        self.rad = rad
+        self.col = col
+
+    def draw_flake(self):
+        arcade.draw_circle_filled(self.pos_x, self.pos_y, self.rad, self.col)
+
+    def update_flake(self):
+        self.pos_y -= self.dy
+        self.pos_x += self.dx
+
+        if self.pos_y < self.rad:
+            self.pos_y = random.randint(600, 700)
+            self.pos_x = random.randint(0, 600)
+            self.rad = random.randint(1, 3)
+
+        if self.pos_x - self.rad > SW or self.pos_x < self.rad:
+            self.dy = random.random() + random.randint(1, 4)
+            self.dx = random.random() + random.randint(-3, 3)
+            self.pos_y = random.randint(600, 700)
+            self.pos_x = random.randint(0, 600)
+            self.rad = random.randint(1, 3)
+
+
+class MyGame(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        arcade.set_background_color(arcade.color.BLACK)
+        self.flake_list = []
+        for i in range(500):
+            x = random.randint(0, 600)
+            y = random.randint(0, 600)
+            dy = random.random() + random.randint(1, 3)
+            dx = random.random() + random.randint(-2, 2)
+            rad = random.randint(1, 3)
+            if i == 1:
+                self.flake = Snowflake(x, y, dy, dx, rad, arcade.color.RED)
+            else:
+                self.flake = Snowflake(x, y, dy, dx, rad, arcade.color.WHITE)
+            self.flake_list.append(self.flake)
+
+    def on_draw(self):
+        arcade.start_render()
+        for flake in self.flake_list:
+            flake.draw_flake()
+        arcade.draw_rectangle_filled(300, 300, 600, 10, arcade.color.BOLE)
+        arcade.draw_rectangle_filled(300, 300, 10, 600, arcade.color.BOLE)
+
+    def on_update(self, dt):
+        for flake in self.flake_list:
+            flake.update_flake()
+
+
+def main():
+    window = MyGame(SW, SH, "Snowfall")
+    arcade.run()
+
+
+if __name__=="__main__":
+    main()
---
+++ idea.py
@@ -0,0 +1,55 @@
+import arcade
+
+SW = 600
+SH = 600
+
+
+class Ball:
+    def __init__(self, pos_x, pos_y, dx, dy, rad, col,):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.dx = dx
+        self.dy = dy
+        self.rad = rad
+        self.col = col
+
+    def draw_ball(self):
+        arcade.draw_circle_filled(self.pos_x, self.pos_y, self.rad, self.col)
+
+    def update_ball(self):
+        # self.pos_x, self.pos_y = arcade.rotate_point(self.pos_x, self.pos_y, 300, 300, 359)
+        self.pos_x += self.dx
+        self.pos_y += self.dy
+
+        if self.pos_x > 500 or self.pos_y > 500:
+            self.dx *= -1
+            self.dy *= -1
+        if self.pos_x < 100 or self.pos_y < 100:
+            self.dx *= -1
+            self.dy *= -1
+
+
+class MyGame(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        arcade.set_background_color(arcade.color.ASH_GREY)
+        self.ball = Ball(300, 300, 3, 3, 15, arcade.color.BLUE)
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_point(300, 300, arcade.color.BLACK, 15)
+        arcade.draw_line(100, 300, 500, 300, arcade.color.BLACK)
+        arcade.draw_line(300, 100, 300, 500, arcade.color.BLACK)
+        self.ball.draw_ball()
+
+    def on_update(self, dt):
+        self.ball.update_ball()
+
+
+def main():
+    window = MyGame(SW, SH, "Drawing Example")
+    arcade.run()
+
+
+if __name__=="__main__":
+    main()
